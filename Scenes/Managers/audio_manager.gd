634ad3ce4d5e914401790d6f35c1f097d0606eca
super-SuPer-dev <57047225+super-SuPer-dev@users.noreply.github.com>
2026-08-04 02:00:51 +0700

    add basic function and scene
    + basic menu scene
    + basic play movement
    + test scene
    + basic HUD
    + AudioManager, GameManager
    + some 2d assets and sfx

---
+++ Scenes/Managers/audio_manager.gd
@@ -0,0 +1,3 @@
+extends Node
+
+@onready var click_sfx := $ClickSfx
---
+++ Scenes/test_hub.gd
@@ -0,0 +1,18 @@
+extends Control
+
+
+
+func _on_w_pressed() -> void:
+	AudioManager.click_sfx.play()
+
+
+func _on_a_pressed() -> void:
+	AudioManager.click_sfx.play()
+
+
+func _on_s_pressed() -> void:
+	AudioManager.click_sfx.play()
+
+
+func _on_d_pressed() -> void:
+	AudioManager.click_sfx.play()
