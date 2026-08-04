bb1b9d62fd6c4a0aa9e61dd6534cda90b08971a8
super-SuPer-dev <57047225+super-SuPer-dev@users.noreply.github.com>
2026-08-05 01:15:14 +0700

    add arrows ui and tileset

--- Actors/player.gd
+++ Actors/player.gd
@@ -1,11 +1,35 @@
 extends CharacterBody2D
 
 @export var speed = 200
+@export var god_mode : bool = false
 
 func get_input():
 	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = input_direction * speed
+	
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("run") and god_check():
+		speed = 800
+	elif event.is_action_released("run") and god_check():
+		speed = 200
+	elif event.is_action_pressed("god"):
+		god_mode = !god_mode
+		god_check()
+
+func _ready() -> void:
+	god_check()
+
+func god_check() -> bool:
+	if god_mode:
+		$CollisionShape2D.disabled = true
+	elif !god_mode:
+		$CollisionShape2D.disabled = false
+	return god_mode
 
 func _physics_process(_delta):
 	get_input()
 	move_and_slide()
+	if velocity.x < 0:
+		$Icon.flip_h = true
+	elif velocity.x > 0:
+		$Icon.flip_h = false
--- Scenes/test_hub.gd
+++ Scenes/test_hub.gd
@@ -1,18 +1,70 @@
 extends Control
 
+@onready var up_label : Label = $MoveUpBtn/Label
+@onready var left_label : Label = $MoveLeftBtn/Label
+@onready var down_label : Label = $MoveDownBtn/Label
+@onready var right_label : Label = $MoveRightBtn/Label
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("move_up"):
+		set_move_up_visual(up_label, true)
+	elif event.is_action_released("move_up"):
+		set_move_up_visual(up_label, false)
+	
+	elif event.is_action_pressed("move_left"):
+		set_move_up_visual(left_label, true)
+	elif event.is_action_released("move_left"):
+		set_move_up_visual(left_label, false)
+		
+	elif event.is_action_pressed("move_down"):
+		set_move_up_visual(down_label, true)
+	elif event.is_action_released("move_down"):
+		set_move_up_visual(down_label, false)
+	
+	elif event.is_action_pressed("move_right"):
+		set_move_up_visual(right_label, true)
+	elif event.is_action_released("move_right"):
+		set_move_up_visual(right_label, false)
 
 
-func _on_w_pressed() -> void:
-	AudioManager.click_sfx.play()
+func set_move_up_visual(label: Label, is_pressed: bool) -> void:
+	if is_pressed:
+		label.modulate = Color()
+	else:
+		label.modulate = Color(1.0, 1.0, 1.0, 1.0)
 
+# MoveUp Button
+func _on_move_up_btn_button_down() -> void:
+	set_move_up_visual(up_label, true)
+	Input.action_press("move_up")
 
-func _on_a_pressed() -> void:
-	AudioManager.click_sfx.play()
+func _on_move_up_btn_button_up() -> void:
+	set_move_up_visual(up_label, false)
+	Input.action_release("move_up")
 
+# MoveLeft Button
+func _on_move_left_btn_button_down() -> void:
+	set_move_up_visual(left_label, true)
+	Input.action_press("move_left")
 
-func _on_s_pressed() -> void:
-	AudioManager.click_sfx.play()
+func _on_move_left_btn_button_up() -> void:
+	set_move_up_visual(left_label, false)
+	Input.action_release("move_left")
 
+# MoveDown Button
+func _on_move_down_btn_button_down() -> void:
+	set_move_up_visual(down_label, true)
+	Input.action_press("move_down")
 
-func _on_d_pressed() -> void:
-	AudioManager.click_sfx.play()
+func _on_move_down_btn_button_up() -> void:
+	set_move_up_visual(down_label, false)
+	Input.action_release("move_down")
+
+# MoveRight Button
+func _on_move_right_btn_button_down() -> void:
+	set_move_up_visual(right_label, true)
+	Input.action_press("move_right")
+
+func _on_move_right_btn_button_up() -> void:
+	set_move_up_visual(right_label, false)
+	Input.action_release("move_right")
